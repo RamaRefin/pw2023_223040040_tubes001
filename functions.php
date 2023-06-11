c77d39c8347e1e1c85b823703536ef83ad9049f6
ramarefin <rama.223040040@mail.unpas.ac.id>
2023-06-11 13:33:00 +0700

    menambahkan fitur registrasi

--- functions.php
+++ functions.php
@@ -153,4 +153,48 @@ function cari($keyword)
 }
 
 
+
+function registrasi($data)
+{
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+    // username sudah ada atau belum
+
+    $result =  mysqli_query($conn, "SELECT username FROM admin WHERE username = '$username'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+                alert('username sudah terdaftar!')
+                </script>";
+
+        return false;
+    }
+
+
+    // cek konfir passowrd
+    if ($password !== $password2) {
+        echo "<script>
+            alert('konfirmasi password tidak sesuai!');
+            </script>";
+        return false;
+    }
+
+    //  enskripsi password
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+
+    // tambahkan user baru ke database
+    mysqli_query($conn, "INSERT INTO admin VALUES('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+}
+
+
+
+
+
 ?>
